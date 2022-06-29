bd53cb400b07803b807c36ebbd3ca0e1197b4c56
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-06-29 13:03:02 -0400

    added models

--- DeskHubSharpRevised/Models/Branch.cs
+++ DeskHubSharpRevised/Models/Branch.cs
@@ -2,5 +2,13 @@ namespace DeskHubSharpRevised.Models;
 
 public class Branch
 {
-    
+    public string name { get; set; }
+
+    public Commit commit { get; set; }
+
+    public class Commit
+    {
+        public string sha { get; set; }
+        public string url { get; set; }
+    }
 }
--- DeskHubSharpRevised/Models/License.cs
+++ DeskHubSharpRevised/Models/License.cs
@@ -2,5 +2,24 @@ namespace DeskHubSharpRevised.Models;
 
 public class License
 {
-    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string key { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string spdx_id { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string url { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public string node_id { get; set; }
 }
