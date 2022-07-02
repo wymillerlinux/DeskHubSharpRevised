dc34b7eccd23651ffc91652cda43fb834269b3b2
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-07-01 23:01:08 -0400

    added branch model

---
+++ ModelsLib/Branch.fs
@@ -0,0 +1,9 @@
+﻿namespace DeskHubSharpRevised.Models
+
+type Branch() =
+    member this.name: string = null
+    member this.commit with get(): obj = null
+    
+    type Commit() =
+        member this.sha with get(): string = null
+        member this.url with get(): string = null
