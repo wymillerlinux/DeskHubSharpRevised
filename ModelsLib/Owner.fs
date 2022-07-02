7caaa6c00b1e79fc88be26df605438c72281130a
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-07-01 23:01:28 -0400

    added owner model

---
+++ ModelsLib/Owner.fs
@@ -0,0 +1,21 @@
+namespace DeskHubSharpRevised.Models
+
+type Owner() =
+    member this.login with get() = null
+    member this.id with get() = null
+    member this.node_id with get() = null
+    member this.avatar_url with get() = null
+    member this.gravatar_url with get() = null
+    member this.html_url with get() = null
+    member this.followers_url with get() = null
+    member this.following_url with get() = null
+    member this.gists_url with get() = null
+    member this.starred_url with get() = null
+    member this.subscriptions_url with get() = null
+    member this.organizations_url with get() = null
+    member this.repos_url with get() = null
+    member this.events_url with get() = null
+    member this.received_events_url with get() = null
+    member this.types with get() = null
+    member this.site_admin with get() = null
+    
