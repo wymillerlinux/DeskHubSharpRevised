f119b44551a319962f2fcca175fd2cb471073f7b
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-07-01 23:01:20 -0400

    added license model

---
+++ ModelsLib/License.fs
@@ -0,0 +1,8 @@
+namespace DeskHubSharpRevised.Models
+
+type License() =
+    member this.key with get() = null
+    member this.name with get() = null
+    member this.spdx_id with get() = null
+    member this.url with get() = null
+    member this.node_id with get() = null
